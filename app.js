2a419bbae86320daffdac166a80621f6e544ae9b
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-18 17:46:44 +1200

    changes

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('Hello World'));
+//app.get('/', (req, res) => res.send('Hello World'));
 
 // use Routes
 app.use('/api/books', books);
