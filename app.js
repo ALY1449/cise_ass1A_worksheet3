bc9619976a1d74be80da8eed5a5198e4b306af69
Alyssa Pausanos <alyssapausanos@Alyssas-MacBook-Pro-2.local>
2022-08-18 17:32:46 +1200

    heroku changes

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const connectDB = require('./config/db');
 var cors = require('cors');
 
+// Accessing the path module
+const path = require("path");
+
 const app = express();
 
 // routes
@@ -17,17 +20,20 @@ app.use(cors({ origin: true, credentials: true }));
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-//middleware
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.get('/', (req, res) => res.send('Hello world!'));
+app.get('/', (req, res) => res.send(path));
 
 // use Routes
 app.use('/api/books', books);
 
-// Accessing the path module
-const path = require("path");
+if (process.env.NODE_ENV === "production"|| process.env.NODE_ENV === "staging") {
+    app.use(express.static("mern_a_to_z_client/build"));
+    app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/mern_a_to_z_client/build/index.html"));
+    });
+}
+else{
+  app.get('/', (req, res) => res.send('API running on port ${port}'));
+}
 
 // Step 1:
 app.use(express.static(path.resolve(__dirname, "./mern_a_to_z_client/build")));
@@ -36,6 +42,6 @@ app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./mern_a_to_z_client/build", "index.html"));
 });
 
-const port = process.env.PORT || 8082;
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
